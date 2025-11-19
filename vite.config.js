d65dabcde60f9b724986f4039988ba32e1f4bfcc
TatiYas <iasinovska_lawyer@ukr.net>
2025-11-19 16:44:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,28 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => ({
-  root: 'src',
-  base: '/Portfolio-website-for-a-Full-stack-developer/', // для GitHub Pages
-  build: {
-    outDir: '../dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: glob.sync('./src/*.html'),
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) return 'vendor';
-        },
-        entryFileNames: '[name].js',
-      },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
-}));
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
+});
